8e53ff59d7716cb582f41fca9e7bf30fdedbbcb6
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-25 07:06:39 +0300

    change component folder directory

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
     return (
         <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
-                <Link href="/">
+                <Link href="/public">
                     <Image src="/logo.png" alt="Logo" width={144} height={30}/>
                 </Link>
                 <div className="flex items-center gap-5">
