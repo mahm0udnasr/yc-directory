f031758cd0711d55c54b5ab49e3ca00c987221e1
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-10-06 08:13:55 +0300

    add search logic and get a params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,8 +10,8 @@ export default async function Home({
   searchParams: { query?: string };
 }) {
   const query = (await searchParams).query;
-  // const posts = await client.fetch(STARTUPS_QUERY);
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
   return (
     <>
       <section className="pink_container">
