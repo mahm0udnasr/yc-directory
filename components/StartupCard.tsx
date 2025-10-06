8f6256c5ab897af6e656a83a474055aad68a67ff
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-10-06 08:04:57 +0300

    add type for startup card

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,18 +1,22 @@
-import { auth } from "@/auth";
+/* eslint-disable @next/next/no-img-element */
 import { formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
 
-const StartupCard = ({ post }: { post: StatupTypeCard }) => {
+export type StartupTypeCard = Omit<Startup, "author"> & {author? : Author}
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
+    // _id,
     _createdAt,
     views,
-    author: { _id: authorId, name },
+    author,
     title,
     category,
-    _id,
+    slug,
     image,
     description,
   } = post;
@@ -27,24 +31,24 @@ const StartupCard = ({ post }: { post: StatupTypeCard }) => {
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
-          <Link href={`/startup/${_id}`}>
+          <Link href={`/startup/${slug?.current}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/48x48"
-            alt={`avatar image for ${name}`}
-            width={48}
-            height={48}
-            className="rounded-full"
+            src={author?.image || ""}
+            alt={`avatar image for ${author?.name}`}
+            width={50}
+            height={50}
+            className="w-[50px] h-[50px] rounded-full object-over"
           />
         </Link>
       </div>
-      <Link href={`/startup/${_id}`}>
+      <Link href={`/startup/${slug?.current}`}>
         <p className="startup-card_desc">{description}</p>
         <img
           src={image}
@@ -53,11 +57,11 @@ const StartupCard = ({ post }: { post: StatupTypeCard }) => {
         />
       </Link>
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}`}>
+        <Link href={`/?query=${category?.toLowerCase()}`}>
           <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup-card_btn" asChild>
-          <Link href={`/startup/${_id}`}>Details</Link>
+          <Link href={`/startup/${slug?.current}`}>Details</Link>
         </Button>
       </div>
     </li>
