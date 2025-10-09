035abb06b39ea68e982aefb49be6a69f4e3a3b7d
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-10-09 14:49:55 +0300

    enhance and fix some bug

--- auth.ts
+++ auth.ts
@@ -4,45 +4,6 @@ import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
 
-// export const { handlers, signIn, signOut, auth } = NextAuth({
-//   providers: [GitHub],
-//   callbacks: {
-//     async signIn({
-//       user: { name, email, image },
-//       profile: { id, login, bio },
-//     }) {
-//       const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-//         id,
-//       });
-//       if (!existingUser) {
-//         await writeClient.create({
-//           _type: "author",
-//           id,
-//           name,
-//           username: login,
-//           email,
-//           image,
-//           bio: bio || "",
-//         });
-//       }
-//       return true;
-//     },
-//     async JWT({ token, account, profile }) {
-//       if (account && profile) {
-//         const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-//           id: profile?.id,
-//         });
-//         token.id = user._id;
-//       }
-//       return token;
-//     },
-//     async Session({ session, token }) {
-//       Object.assign(session, { id: token.id });
-//       return session;
-//     },
-//   },
-// });
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
